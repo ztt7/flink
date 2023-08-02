358695b0c834f39596f4ea3b59a57feffb482b61
ztt <ztt@163.com>
2023-08-02 14:46:06 +0800

    8.2

---
+++ flink_course/src/main/java/exec1/wc/BatchWordCount.java
@@ -0,0 +1,39 @@
+package exec1.wc;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.operators.UnsortedGrouping;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class BatchWordCount {
+    public static void main(String[] args) throws Exception {
+        // 1. 创建执行环境
+        ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
+        // 2. 从文件读取数据  按行读取(存储的元素就是每行的文本)
+        DataSource<String> lineDS = environment.readTextFile("input/words.txt");
+        // 3. 转换数据格式
+        FlatMapOperator<String, Tuple2<String, Long>> wordAndOne = lineDS.flatMap(new FlatMapFunction<String, Tuple2<String, Long>>() {
+            @Override
+            public void flatMap(String line, Collector<Tuple2<String, Long>> collector) throws Exception {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    collector.collect(Tuple2.of(word, 1L));
+                }
+            }
+        });
+        // 4. 按照 word 进行分组
+        UnsortedGrouping<Tuple2<String, Long>> wordAndOneUG = wordAndOne.groupBy(0);
+
+        // 5. 分组内聚合统计
+        AggregateOperator<Tuple2<String, Long>> sum = wordAndOneUG.sum(1);
+
+        // 6. 打印结果
+        sum.print();
+
+    }
+}
+
